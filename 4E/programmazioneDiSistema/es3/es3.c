2163790e52db13a64da9b3f015e07d482119729e
obbego <francesco.bego@iisviolamarchesini.edu.it>
2025-03-29 16:51:58 +0100

    es 3 finito

--- 4E/programmazioneDiSistema/es3/es3.c
+++ 4E/programmazioneDiSistema/es3/es3.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <string.h>
 
 /*Si scriva un’applicazione concorrente che presenti la seguente interfaccia:
 
@@ -23,11 +24,12 @@ stringhe trovate nel file.*/
 int main(int argc, char *argv[])
 {
     int nParoleTrovate = 0;
-    int p1p2;
-    char *stringa;
+    int p1p0[2];
+    int pid;
+    char buff[10000], stringa[1000000];
 
-    pipe(p1p2);
-    
+    pipe(p1p0);
+
     if (argc != 2)
     {
         printf("Numero parametri errato\n");
@@ -38,14 +40,35 @@ int main(int argc, char *argv[])
     {
         printf("cercaStringe %s\n", argv[1]);
 
-        printf("Inserisci stringa da cercare\n 'fine' per terminare\n");
+        printf("Inserisci stringa da cercare\n'fine' per terminare\n");
         scanf("%s", stringa);
 
-        if (stringa == "fine") {
-            printf("Programma terminato: %d parole trovate", &nParoleTrovate);
-            return 0;
-        } else {
+        if (strcmp(stringa, "fine") == 0)
+        {
+            close(p1p0[0]);
+            close(p1p0[1]);
 
+            printf("\n\nProgramma terminato: %d parole trovate\n", nParoleTrovate);
+            return 0;
+        }
+        else
+        {
+            pid = fork();
+
+            if (pid == 0) // figlio
+            {
+                close(p1p0[0]);
+                close(1);
+                dup(p1p0[1]);
+                close(p1p0[1]);
+
+                execl("/usr/bin/grep", "grep", "-c", stringa, argv[1], (char *)0);
+                return -1;
+            }
+
+            read(p1p0[0], &buff, sizeof(buff));
+            printf("Il file ha %d '%s' \n\n", atoi(buff), stringa);
+            nParoleTrovate += atoi(buff);
         }
     }
 }
