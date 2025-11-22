7f9aa5e23cdabc25fc36a4c20d74246d79b907f9
Francesco Bego <francesco.bego@iisviolamarchesini.edu.it>
2025-11-22 12:22:41 +0100

    Es morra cinese

---
+++ 5E/socket/es4-MorraCinese/Server.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+//Scegli (0: Sasso, 1: Carta, 2: Forbice, 9 per uscire): 9
+
+
+//Metodo per decidere chi vince: 0 = pareggio, 1 = vince g1(server), 2 = vince g2(client)
+int whoWins(int sceltaG1, int sceltaG2){
+    if ()
+}
